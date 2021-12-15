9fa83e14fc2bda866502cae518d24e1fe2964bd8
Rahma Abdullahi <82878278+rahmaabdul@users.noreply.github.com>
2021-12-15 18:47:21 +0000

    changes

---
+++ js/questions.js
@@ -0,0 +1,53 @@
+// const questions = [
+//   {
+//     title: "What is an Array?",
+//     choices: ["an array is..", "it is..", "it happens to be... "],
+//     answer: "an array is..",
+//   },
+
+//   {
+//     title: "What does HTML stand for?",
+//     choices: [
+//       "HyperText Markup Language",
+//       "HyperTone Make Language",
+//       "HowText Makes Long,",
+//       "HiddenText mark Language",
+//     ],
+//     answer: "HyperText Markup Language",
+//   },
+
+//   {
+//     title: "What does CSS stand for?",
+//     choices: [
+//       "Cascading style sheets",
+//       "Code style sheet",
+//       "Colour system static",
+//       "computer style sheet",
+//     ],
+//     answer: "Cascading Style sheets",
+//   },
+
+//   {
+//     title: "What is JavaScript?",
+//     choices: [
+//       "A scrypting language used to create and control dynamic web content",
+//       "A server for hosting applications",
+//       "A guide to css and HTML",
+//       "A font name",
+//     ],
+//     answer:
+//       "A scrypting language used to create and control dynamic web content",
+//   },
+
+//   {
+//     title: "",
+//     choices: "",
+//     answer: "",
+//   },
+
+//   {
+//     title: "",
+//     choices: "",
+//     answer: "",
+//   },
+// ];

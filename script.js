d1238800efe6dd9e0b97edbff3a0fcdb96b25681
Rahma Abdullahi <82878278+rahmaabdul@users.noreply.github.com>
2021-12-15 18:52:58 +0000

    corrections

--- script.js
+++ script.js
@@ -1,108 +1,101 @@
 const quizData = [
-   { 
-
+  {
     question: "What does HTML stand for?",
     a: "HyperText Markup Language",
     b: "HyperTone Markup Language",
     c: "HyperText Markup Log",
     d: "HiddenText Mark Language",
     correct: "a",
-}
-{
+  },
+
+  {
     question: "What does CSS stand for?",
-    a:  "Code Style Sheets",
+    a: "Code Style Sheets",
     b: "Cascading style sheets",
     c: "Colour Static Systems",
     d: "Computer Style Sheet",
     correct: "b",
-}
- {
-     question: "What is JavaScript?",
-     a:"A font name",
-     b:"A guide to css and HTML",
-     c:"A server for hosting applications",
-     d:"A scrypting language used to create and control dynamic web content",
-     correct: "d",
- }
+  },
 
- {
-    question: "Which language runs in a web browser?",
-    a:"Java",
-    b:"C",
-    c:"Python",
-    d:"javascript",
+  {
+    question: "What is JavaScript?",
+    a: "A font name",
+    b: "A guide to css and HTML",
+    c: "A server for hosting applications",
+    d: "A scrypting language used to create and control dynamic web content",
     correct: "d",
-}
+  },
+
+  {
+    question: "Which language runs in a web browser?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "javascript",
+    correct: "d",
+  },
 ];
 
-const quiz=document.getElementById('quiz')
-const answerEls = document.querySelectorAll('.answer')
-const questionEl = document.getElementById('question')
-const a_text = document.getElementById('a_text')
-const b_text = document.getElementById('b_text')
-const c_text = document.getElementById('c_text')
-const d_text = document.getElementById('d_text')
-const submitBtn = document.getElementById('submit')
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
 
-let currentQuiz = 0
-let score = 0
+let currentQuiz = 0;
+let score = 0;
 
-loadQuiz()
+loadQuiz();
 
 function loadQuiz() {
-    deselectAnswers()
+  deselectAnswers();
 
-    const currentQuizData = quizData[currentQuiz]
+  const currentQuizData = quizData[currentQuiz];
 
-    questionEl.innerText = currentQuizData.question
-    a_text.innerText = currentQuizData.a
-    b_text.innerText = currentQuizData.b
-    c_text.innerText = currentQuizData.c
-    d_text.innerText = currentQuizData.d
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
 }
 
 function deselectAnswers() {
-    answerEls.forEach(answerEls => answerEls.checked = false) 
+  answerEls.forEach((answerEls) => (answerEls.checked = false));
 }
 
-function getSelected(){
-    let answerEls
-    answerEls.forEach(answerEl =>{
-        if(answerEl.checked) {
-            answer = answerEl.id
-        }
-     })
-     return answer
+function getSelected() {
+  let answerEls;
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
 }
 
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
 
-submitBtn.addEventListener('click', () => {
-    const answer = getSelected()
-    if(answer) {
-        if(answer === quizData[currentQuiz].correct) {
-            score++
-        }
+    currentQuiz++;
 
-        currentQuiz++
-
-        if(currentQuiz < quizData.length) {
-            loadQuiz()
-        } else {
-            quiz.innerHtml = `
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHtml = `
             <h2> you answered ${score}/${quizData.length} questions correctly</h2>
 
             <button onclick="location.reload()".reload</button>
-            `
-        }
+            `;
     }
-})
-
-
-
-
-
-
-
+  }
+});
 
 // let i = 0;
 // let score = 0;
@@ -111,7 +104,6 @@ submitBtn.addEventListener('click', () => {
 // const newMessage = document.querySelector("#message");
 // let localScores;
 // let scoreLest = [];
-
 
 // const choiceA = document.getElementById("choiceA");
 // const choiceB = document.getElementById("choiceB");
